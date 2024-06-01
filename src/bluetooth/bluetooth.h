1407d995c6647d56f3c1e4d6258f5f8970543650
nazhon <nazargon2003@gmail.com>
2024-06-01 11:25:16 +0300

    Add BluetoothCore

---
+++ src/bluetooth/bluetooth.h
@@ -0,0 +1,5 @@
+class BluetoothCore
+{
+public:
+    void initialize();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,19 @@
 #include <AsyncUDP.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
-#define INT2POINTER(a) ((char *)(intptr_t)(a))
-
-void drawCentreString(const char *buf, uint8_t textSize, int cursorX, int cursorY)
-{
-  int16_t x1, y1;
-  uint16_t w, h;
-
-  display.setTextSize(textSize); // 6x8
-  display.getTextBounds(buf, cursorX, cursorY, &x1, &y1, &w, &h);
-  display.setCursor((SCREEN_WIDTH - w) / 2, (SCREEN_HEIGHT - h) / 2);
-  display.print(buf);
-}
+#include "bluetooth/bluetooth.h"
 
 void setup()
 {
   Serial.begin(115200);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ;
-  }
+  BluetoothCore bluetoothCore;
+
+  Serial.println("Starting BLE work!");
+  bluetoothCore.initialize();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop()
 {
-  display.clearDisplay();
-
-  display.setTextColor(SSD1306_WHITE);
-  drawCentreString("200", 5, 0, 0);
-
-  display.display();
+  // delay(2000);
 }
