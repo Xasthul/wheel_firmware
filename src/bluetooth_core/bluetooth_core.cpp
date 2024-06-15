edaf31487d86586f36e88ea13592339917557025
nazhon <nazargon2003@gmail.com>
2024-06-15 14:04:17 +0300

    Export bleServerName to define in bluetooth core

--- src/bluetooth_core/bluetooth_core.cpp
+++ src/bluetooth_core/bluetooth_core.cpp
@@ -5,6 +5,8 @@
 #include "bluetooth_core.h"
 #include "bluetooth_connection_callbacks/bluetooth_connection_callbacks.h"
 
+#define bleServerName "Wheel"
+
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
@@ -12,7 +14,7 @@ void BluetoothCore::initialize()
 {
     Serial.println("Starting BLE work..");
 
-    BLEDevice::init("The best wheel");
+    BLEDevice::init(bleServerName);
     BLEServer *pServer = BLEDevice::createServer();
 
     pServer->setCallbacks(new BluetoothConnectionCallbacks());
